1346fdab16e3b8556fd926db8ab7958a94269752
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2024-08-07 16:43:08 +0530

    feat:real time liveblock

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,25 +1,5 @@
-import { Editor } from "@/components/editor/Editor";
-import Header from "@/components/Header";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import React from "react";
-
 const Documents = () => {
-  return (
-    <div>
-      <Header>
-        <div className="flex w-fit itemes-center justify-center gap-2">
-          <p className="document-title">Share</p>
-        </div>
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </Header>
-      <Editor />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Documents;
