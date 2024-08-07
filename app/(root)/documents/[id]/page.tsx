89f487b6864ce508cdb8aa5eb93a14f831af9846
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2024-08-08 01:53:42 +0530

    feat:AddDocumentBtn

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,5 +1,11 @@
+import CollaborativeRoom from "@/components/CollaborativeRoom";
+
 const Documents = () => {
-  return <div></div>;
+  return (
+    <main className="w-full flex flex-col items-center">
+      <CollaborativeRoom />
+    </main>
+  );
 };
 
 export default Documents;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,42 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
+import Header from "@/components/Header";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import { currentUser } from "@clerk/nextjs/server";
+import AddDocumentBtn from "@/components/AddDocumentBtn";
 
-const Home = () => {
+const Home = async () => {
+  const clerkUser = await currentUser();
+  const documents = [];
   return (
-    <div>
-      <Button>Click Me</Button>
-    </div>
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          Notification
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+      {documents.length > 0 ? (
+        <div className="flex flex-col gap-4"></div>
+      ) : (
+        <div className="document-list-empty">
+          <Image
+            src="/assets/icons/doc.svg"
+            alt="Document"
+            width={40}
+            height={40}
+            className="mx-auto"
+          />
+          <AddDocumentBtn
+            userId={clerkUser!.id}
+            email={clerkUser!.emailAddresses[0].emailAddress}
+          />
+        </div>
+      )}
+    </main>
   );
 };
 
